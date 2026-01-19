5eec4c818beb2f5f97e2484e6b98c504f9b32946
safayet <shafayethossain9009@gmail.com>
2026-01-20 02:38:44 +0600

    2nd update

--- oops1.py
+++ oops1.py
@@ -4,9 +4,10 @@ class employee:
     #special mehod/magic method/dunder method-constructor
     def __init__(self):
         self.id=123
-        self.salary=50000
+        self.salary=500000
         self.designation='SWE'
-
+    def travel(self,destination):
+        print("Employee is traveling to", destination)
 
 #create an obj/instance of the class
 sam=employee()
@@ -14,3 +15,4 @@ sam=employee()
 print(sam.id)
 print(sam.salary)
 print(sam.designation)
+sam.travel("New York")
