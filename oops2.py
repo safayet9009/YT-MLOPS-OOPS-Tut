8d11c6aae4b4db6eb993569ddd722145c5a4df66
safayet <shafayethossain9009@gmail.com>
2026-01-22 21:40:42 +0600

    6th

---
+++ oops2.py
@@ -0,0 +1,18 @@
+#initialize a class
+
+class employee:
+    #special mehod/magic method/dunder method-constructor
+    def __init__(self):
+        self.id=777
+        self.salary=60000
+        self.designation='MLopsEngineer'
+    def travel(self,destination):
+        print("Employee is traveling to", destination)
+
+#create an obj/instance of the class
+sam=employee()
+
+print(sam.id)
+print(sam.salary)
+print(sam.designation)
+sam.travel("New York")
