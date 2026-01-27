446f7061a35e71f794fdd78c72c20c92ba626657
safayet <shafayethossain9009@gmail.com>
2026-01-28 01:19:37 +0600

    9th update

--- oops3.py
+++ oops3.py
@@ -1,3 +1,4 @@
+'''
 class Person:
   def __init__(self, name):
     self.name = name
@@ -11,4 +12,67 @@ class Person:
 
 p1=Person('Tobias')    
 p1.welcome()
+'''
+'''
+class person:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+p1=person('shafayet',25)
+p1.age=40
+del p1.age
+print(p1.name)
+'''
+'''
+class person:
+    species='human'
 
+    def __init__(self,name):
+        self.name=name
+
+p1=person("emily")
+p2=person('tobias')
+
+print(p1.species)
+print(p2.species)
+
+print(p1.name)
+'''
+'''
+class person:
+    lastname='Jackson'
+
+    def __init__(self,name):
+        self.name=name
+        
+p1=person('Emil')
+p2=person('Tobias')
+print(p1.lastname)
+
+person.lastname='Hansen'
+print(p1.lastname)
+print(person.lastname)
+print(p2.lastname)
+print(p1.name)
+'''
+'''
+class calculator:
+    def add(self,a,b):
+        return a+b
+    def mul(self,a,b):
+        return a*b
+calc=calculator()
+
+print(calc.add(5,3)) 
+print(calc.mul(23,4))
+'''
+class person:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+
+    def greet(self):
+        return f'{self.name} is {self.age} years old'   
+p1=person("John", 30)
+
+print(p1.greet())
