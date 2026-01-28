6e5354995cb84bc21044c3a26a0fc439b4089377
safayet <shafayethossain9009@gmail.com>
2026-01-29 02:32:35 +0600

    basic structure ready for this mini project

---
+++ mini_project.py
@@ -0,0 +1,24 @@
+class chatbook:
+
+    def menu(self):
+        user_input=input("""welcome to chatbook!! How would you like to process
+                         1.press 1 to signup
+                         2.press 2 to signin
+                         3.press 3 to write a post
+                         4. Press 4 to message a friend
+                         5. Press any other key to exit""")
+    
+
+        if user_input == '1':
+            pass
+        elif user_input == '2':
+            pass
+        elif user_input == '3':
+            pass
+        elif user_input == '4':
+            pass
+        else:
+            print("Exiting...")
+
+
+    
