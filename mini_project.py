cfa7e5ce8c29ef3fc92096df71f859ca2ce39828
safayet <shafayethossain9009@gmail.com>
2026-01-29 03:35:28 +0600

    signup and signin feature add

--- mini_project.py
+++ mini_project.py
@@ -1,24 +1,60 @@
 class chatbook:
+    def __init__(self):
+        self.username=''
+        self.password=''
+        self.loggedin=False
+        self.menu()
 
     def menu(self):
-        user_input=input("""welcome to chatbook!! How would you like to process
+        user_input=input("""welcome to chatbook!! How would you like to process\n'
                          1.press 1 to signup
                          2.press 2 to signin
                          3.press 3 to write a post
-                         4. Press 4 to message a friend
-                         5. Press any other key to exit""")
+                         4.Press 4 to message a friend
+                         5.Press any other key to exit' """)
     
 
         if user_input == '1':
-            pass
+            self.signup()
         elif user_input == '2':
-            pass
+            self.signin()
         elif user_input == '3':
             pass
         elif user_input == '4':
             pass
         else:
-            print("Exiting...")
+            exit()
+
+
+    def signup(self):
+        email=input("Enter your email: ")
+        pwd=input ("Enter your password: ")
+        self.username=email
+        self.password=pwd
+        print("Signup successful!")
+        print('\n')
+        self.menu()
+
+    def signin(self):
+        if self.username=='' and self.password=='':
+            print("please signup first by pressing 1")
+        else:
+            uname=input("Enter your username/email: ")
+            pwd=input("Enter your password: ")
+            if self.username==uname and self.password==pwd:
+                print("Signin successful!")
+                self.loggedin=True
+            else:
+                print("please input correct credentials..")
+
+            print('\n')
+
+            self.menu()
+
+
+
+
+user1=chatbook()
 
 
     
